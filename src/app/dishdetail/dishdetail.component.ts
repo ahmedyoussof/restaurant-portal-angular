60ee49c97a26e3a4e5134e51fc9488f61f54e4e5
Ahmed Youssof <ahmedyoussof92@gmail.com>
2018-01-26 21:32:23 +0200

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -10,7 +10,8 @@ import { Comment } from '../shared/comment';
 
 import 'rxjs/add/operator/switchMap';
 
-import { trigger, state, style, animate, transition } from '@angular/animations';
+
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 
 
@@ -19,18 +20,14 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.1s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
