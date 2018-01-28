60d8d9efa83414499ab7203302dab28002f0c4f7
Ahmed Youssof <ahmedyoussof92@gmail.com>
2018-01-28 18:38:35 +0200

    Assignment 4 task 1,2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { Leader } from '../shared/leader';
 
@@ -23,11 +23,13 @@ export class AboutComponent implements OnInit {
 
   leaders: Leader[];
   selectedLeader: Leader;
+  errMess: string;
 
-  constructor(private leaderService: LeaderService) { }
+  constructor(private leaderService: LeaderService,
+    @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
-    this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders);
+    this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders, errmess => this.errMess=errmess);
   }
 
   onSelect(leader: Leader) {
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -7,22 +7,24 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 
+import 'rxjs/add/operator/catch';
+import {Restangular, RestangularModule } from 'ngx-restangular';
 
 @Injectable()
 export class PromotionService {
 
-  constructor() { }
+  constructor(private restangular: Restangular) { }
 
   getPromotions(): Observable<Promotion[]> {
-    return Observable.of(PROMOTIONS).delay(2000);
+    return this.restangular.all('promotion').getList();
   }
 
   getPromotion(id: number): Observable<Promotion> {
-    return Observable.of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).delay(2000);
+    return this.restangular.one('promotion', id).get();
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return Observable.of(PROMOTIONS.filter((promo) => promo.featured)[0]).delay(2000);
+    return this.restangular.all('promotions').getList({featured: true}).map(promo => promo[0]);
   }
 
 }
